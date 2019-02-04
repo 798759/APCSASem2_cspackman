c93495f820a3ebdeb8031d4547a4908de1e4f608
cspackman <42615796+cspackman@users.noreply.github.com>
2019-02-04 10:27:57 -0800

    hi

---
+++ Gui/CanvasComponent.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+/**
+ * Write a description of class CanvasComponent here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CanvasComponent
+{
+    private int xPosition;
+    private int yPosition;
+    private int width;
+    private int height; 
+    public CanvasComponent(int width, int height)
+    {
+        
+        
+    }
+ 
+}
---
+++ Gui/MySwingApplication.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class MySwingApplication here.
+ *
+ * @author (Connor Spackman)
+ * @version (2/4/2019)
+ */
+import javax.swing.*;
+public class MySwingApplication implements Runnable
+{
+ JFrame jFrame;//First class name, second instance variable name
+    public static void main() {
+MySwingApplication mySwingApplicationSwing = new MySwingApplication();
+javax.swing.SwingUtilities.invokeLater(mySwingApplicationSwing);
+}
+public  void run(){
+    jFrame = new JFrame("Window");
+    jFrame.setSize(400, 400);
+    jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    CanvasComponent  component= new CanvasComponent(100,100);
+    jFrame.setVisible(true);
+    
+}
+
+}
