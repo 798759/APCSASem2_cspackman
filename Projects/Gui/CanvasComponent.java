e3a9b21ff4294dc59d92543a06d71a13877de382
cspackman <42615796+cspackman@users.noreply.github.com>
2019-02-12 10:19:40 -0800

    m

--- Projects/Gui/CanvasComponent.java
+++ Projects/Gui/CanvasComponent.java
@@ -7,7 +7,7 @@ import java.awt.event.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class CanvasComponent extends JComponent implements MouseListener, MouseMotionListener
+public class CanvasComponent extends JComponent implements MouseListener, MouseMotionListener, ActionListener
 {
     private int xPos;
     private int yPos;
@@ -21,7 +21,12 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     private int colorR;
     private int colorG;
     private int colorB;
-    
+    private int animationDeltaX;
+    private int animationDeltaY;
+    private int gutterX;
+    private int gutterY;
+    private int rectWidth;
+    private int rectHeight;
     public CanvasComponent(int w, int h)
     {
         width=w;
@@ -29,23 +34,29 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
         setSize(width,height);   
         xPos=150;
         yPos=150;
+        rectWidth=90;
+        rectHeight=100;
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
         colorR=(int)(Math.random()*189)+66;
         colorG=(int)(Math.random()*155)+100;
         colorB=(int)(Math.random()*55)+200;
+        animationDeltaX = 1;
+        animationDeltaY =0;
+        gutterX =10;
+        gutterY=10;
 
     }
     public void paintComponent(Graphics g){
         //colorR=(int)(Math.random()*189)+66;
         //colorG=(int)(Math.random()*155)+100;
         //colorB=(int)(Math.random()*55)+200;
-        System.out.println(colorR);
-        System.out.println(colorG);
-        System.out.println(colorB);
+        //System.out.println(colorR);
+        //System.out.println(colorG);
+        //System.out.println(colorB);
         Color nc = new Color(colorR,colorG,colorB);
         g.setColor(nc);
-        g.fillRect(xPos,yPos, width, height);
+        g.fillRect(xPos,yPos, rectWidth, rectHeight);
     }
     public void mouseClicked(MouseEvent e){
     }
@@ -71,6 +82,21 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     public void mouseExited(MouseEvent e){
     }
     public void mouseMoved(MouseEvent e){
+    }
+    public void actionPerformed(ActionEvent e){
+          Dimension componentSizeDimension= getSize();
+          if(xPos+yPos+gutterX>width){
+             xPos=rectWidth-gutterX;
+             animationDeltaY=1;
+             animationDeltaX=0;
+             rectHeight+=animationDeltaY;
+           }
+           if(xPos+yPos+gutterY>height){
+             xPos=rectWidth-gutterX;
+             animationDeltaY=0;
+             animationDeltaX=-1;
+             rectHeight+=animationDeltaY;
+           }
     }
     public void mouseDragged(MouseEvent e){
         mouseToX=e.getX();
