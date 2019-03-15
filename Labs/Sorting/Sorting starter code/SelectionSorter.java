f89c12c4db725190ef195b35aea8114a9d535ebd
cspackman <42615796+cspackman@users.noreply.github.com>
2019-03-15 10:25:16 -0700

    hg

---
+++ Labs/Sorting/Sorting starter code/SelectionSorter.java
@@ -0,0 +1,111 @@
+
+/**
+ * Write a description of class SelectionSorter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class SelectionSorter extends Sorter
+{
+
+    /**
+     * Constructor for objects of class SelectionSorter
+     */
+    public SelectionSorter()
+    {
+        super();
+
+    }
+
+    public void onePass1(int[] a, int initialLeftIndex, int limitRightIndex) {
+        int pivotValue = a[initialLeftIndex];
+        int rightIndex = limitRightIndex - 1;
+        int leftIndex = initialLeftIndex + 1;
+        while (leftIndex <= rightIndex) {
+            if (less(pivotValue, a[leftIndex])) {
+                while ((rightIndex >= leftIndex) && less(pivotValue, a[rightIndex])) {
+                    rightIndex--;
+                }
+                if (leftIndex < rightIndex) {
+                    swap(a, leftIndex, rightIndex);
+                    rightIndex--;
+                    leftIndex++;
+                }
+            } else {
+                leftIndex++;
+            }
+        }
+        if (initialLeftIndex < leftIndex - 1) {
+            swap(a, initialLeftIndex, leftIndex - 1);
+            onePass(a, initialLeftIndex, leftIndex - 1);
+        }
+        if (leftIndex < limitRightIndex) {
+            onePass(a, leftIndex, limitRightIndex);
+        }
+    }
+
+    private void setPivot(int[] a, int initialLeftIndex, int limitRightIndex) {
+        int elementCount = limitRightIndex - initialLeftIndex;
+        int rightIndex = limitRightIndex - 1;
+        if (elementCount >= 3) {
+            int middleIndex = initialLeftIndex + elementCount/2;
+            int leftValue = a[initialLeftIndex];
+            int rightValue = a[rightIndex];
+            int middleValue = a[middleIndex];
+            if (less(a[rightIndex], a[middleIndex])) {
+                if (less(a[initialLeftIndex], a[rightIndex])) {
+                    swap(a, initialLeftIndex, rightIndex);
+                }
+            }
+            if (less(a[initialLeftIndex], a[middleIndex])) {
+                swap(a, initialLeftIndex, middleIndex);
+            }
+        }
+    }
+
+    public void onePass(int[] a, int initialLeftIndex, int limitRightIndex) {
+        // By eliminating the smart pivot code, sorted arrays produce
+        // worst case behavior
+        // setPivot(a, initialLeftIndex, limitRightIndex);
+        int pivotValue = a[initialLeftIndex];
+        int rightIndex = limitRightIndex - 1;
+        int leftIndex = initialLeftIndex + 1;
+        while (leftIndex <= rightIndex) {
+            while ((leftIndex <= rightIndex) && lessEqual(a[leftIndex], pivotValue)) {
+                leftIndex++;
+            }
+            while ((leftIndex <= rightIndex) && less(pivotValue, a[rightIndex])) {
+                rightIndex--;
+            }
+            if (leftIndex < rightIndex) {
+                swap(a, leftIndex, rightIndex);
+                rightIndex--;
+                leftIndex++;
+            }
+        }
+        if (initialLeftIndex < leftIndex - 1) {
+            swap(a, initialLeftIndex, leftIndex - 1);
+            onePass(a, initialLeftIndex, leftIndex - 1);
+        }
+        if (leftIndex < limitRightIndex) {
+            onePass(a, leftIndex, limitRightIndex);
+        }
+    }
+
+    public void sort(int[] a) {
+        onePass(a, 0, a.length);
+    }
+    public void  Sort(int [] arr){
+        for(int i=0; i<=arr.length-1; i++){
+            int index =i;
+            for(int j=i+1; j<=arr.length; j++){
+                if (arr[j]<=arr[index]){
+                    int temp = arr[j];
+                    arr[j]=arr[index];
+                    arr[index]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
