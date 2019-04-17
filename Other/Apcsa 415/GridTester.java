78aa9cd542c41e049143292ccd0e97b721dcd75b
cspackman <798759@seq.org>
2019-04-16 21:53:19 -0700

    hi

---
+++ Other/Apcsa 415/GridTester.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class GridTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class GridTester
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class GridTester
+     */
+    public GridTester(int rows,int colulm)
+    {
+        int[][] x = new int[rows][colulm];
+    }
+    public void loadOccupied(){
+        //??
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
