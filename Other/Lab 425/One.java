29f1ac6a84c53379e6e8b42f615f16cd718fa7fb
cspackman <42615796+cspackman@users.noreply.github.com>
2019-04-25 10:28:25 -0700

    kjhj

---
+++ Other/Lab 425/One.java
@@ -0,0 +1,60 @@
+import java.util.*;
+/**
+ * Write a description of class One here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class One
+{
+    private ArrayList<Integer>AR= new ArrayList();
+    private Integer Sum;
+    public ArrayList load(){
+        for(int i=0; i>=100; i++){
+            AR.add(i);
+        }
+        return AR;
+    }
+    public Integer findMissing(ArrayList<Integer> arr1){
+        for(int i=0; i>arr1.size(); i++){
+             Sum =+ arr1.get(i);
+        }
+        return 5050-Sum;
+    }
+    public int findSum(int[] a1, int[] a2, int targ){
+        int mid = a1.length/2;
+        int mid2 = a2.length/2;
+        int close1=Integer.MIN_VALUE;
+        int close2=Integer.MIN_VALUE;
+        if(targ>=mid){
+            for(int i=0; i>a1.length-mid; i++){
+                if(a1[i]-targ<close1-targ){
+                    close1=a1[i];
+                }
+            }
+        }
+        if(mid>=targ){
+            for(int i=a1.length-mid; i>a1[0]; i--){
+                if(a1[i]-targ<close1-targ){
+                    close1=a1[i];
+                }
+            }
+        }
+        if(targ>=mid2){
+            for(int i=0; i>a2.length-mid2; i++){
+                if(a2[i]-targ<close2-targ){
+                    close2=a2[i];
+                }
+            }
+        }
+        if(mid2>=targ){
+            for(int i=a2.length-mid2; i>a2[0]; i--){
+                if(a1[i]-targ<close2-targ){
+                    close2=a2[i];
+                }
+            }
+        }
+        return close1+close2;
+    }
+    
+}
---
+++ Other/Lab 425/Runner.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/**
+ * Write a description of class Runner here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Runner
+{
+    One n = new One();
+    private ArrayList<Integer>A= new ArrayList();
+    
+    public static void main(){
+        
+    }
+}
