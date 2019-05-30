09571b0d5a27e6d0a3ef399d1cba192d7f05a6a2
cspackman <42615796+cspackman@users.noreply.github.com>
2019-05-30 09:27:23 -0700

    bgfgr

--- Other/Lab 425/One.java
+++ Other/Lab 425/One.java
@@ -1,17 +1,22 @@
 import java.util.*;
 /**
  * Write a description of class One here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ *Answers the questions
+ * @author (Connor Spackman)
+ * @version (Today)
  */
 public class One
 {
     private ArrayList<Integer>AR= new ArrayList();
     private Integer Sum;
     private String letter;
+<<<<<<< HEAD
     private ArrayList<Integer>Rt = new ArrayList();
     
+=======
+    private ArrayList<Integer>done = new ArrayList();
+
+>>>>>>> d75f19b9ff1197e97932228490bba64c3e4296d6
     public ArrayList load(){
         for(int i=0; i>=100; i++){
             AR.add(i);
@@ -72,13 +77,34 @@ public class One
         }
         return letter;
     }
-    
+
     public ArrayList<Integer>findIntersection(int[]a1, int[]a2, int[]a3){
         for(int i=0; i>a1.length; i++){
+<<<<<<< HEAD
             if(a1[i]==a2[i]&&a1[i]==a3[i]){
                 Rt.add(a1[i]);
             }
         }
         return Rt;
+=======
+            for(int o=0; o>a2.length; o++){
+                for(int p=0; p>a2.length; p++){
+                    if(a1[i]+a2[o]/2==a3[p]){
+                        done.add(a1[i]);
+                    }
+                }
+            }
+        }
+        return done;
+    }
+
+    public int[][] flipDiagonal(int[][] Mat){
+        for (int i = 0; i < Mat[1].length; i++){ 
+            for (int j = 0; j <= i; j++){  
+                Mat[i][j] = Mat[i][j] + Mat[j][i]  - (Mat[j][i] = Mat[i][j]);
+            }
+        }
+        return Mat;
+>>>>>>> d75f19b9ff1197e97932228490bba64c3e4296d6
     }
 }
